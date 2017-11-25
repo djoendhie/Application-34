087e602a0b5c555dd3db72ed3e43069305485728
Djoendhie <ihaandjoe68@Gmail.com>
2017-11-25 21:06:58 +0700

    Aceh

---
+++ Aceh/Aceh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aceh
+//
+//  Created by SMK IDN MI on 11/25/17.
+//  Copyright © 2017 Djoendhie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
